91110d2c007f2c2b0f4ae6846f2f6f32827e4dd0
Ryan <ryanpburnett@yahoo.com>
2021-03-28 11:45:04 -0500

    auto increment on id

--- db/schema.sql
+++ db/schema.sql
@@ -5,19 +5,19 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department (
-	id INT NOT NULL,
+	id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 CREATE TABLE role (
-	id INT NOT NULL,
+	id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
     PRIMARY KEY (id)
 ); 
 CREATE TABLE employee (
-	id INT NOT NULL,
+	id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,

f5533d372e9c660072fd07209c1809953a264a1e
Ryan <ryanpburnett@yahoo.com>
2021-03-28 13:51:19 -0500

    inquirer first prompt working

--- index.js
+++ index.js
@@ -6,17 +6,42 @@ const questions = () => {
     inquirer
         .prompt([
             {
-            // use "input" by default
-            type: "checkbox",  
-            message: chalk.blue("What's up man?"),
+            type: "list",  
+            message: chalk.blue("What would you like to do"),
+            name: "options",
             choices: [
-                {name: "idk nm"},
-                {name: "workin' hard"},
-                {name: "hardly workin'"},
+                "Add department",
+                "View departments",
+                "Update employee roles",
+                "End"
             ],
-            name: "whatsup"
+            
             }
-        ])
+        ]).then(answer => {
+            const { options } = answer;
+            if(options === "Add department") {
+                addDepartment();
+            }else if(options === "View departments"){
+                viewDepartments();
+            }else if(options === "Update employee roles") {
+                updateEmployeeRoles();
+            }else{
+                end();
+            }
+        });
+
+        function addDepartment() {
+            console.log("addDept")
+        }
+        function viewDepartments() {
+            console.log("viewDept")
+        }
+        function updateEmployeeRoles() {
+            console.log("updateEmp")
+        }
+        function end() {
+            console.log("end")
+        }
 }
 
 questions()

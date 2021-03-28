7a6a0fdf6959cc3414104ba267f0e1a1d2aab18a
Ryan <ryanpburnett@yahoo.com>
2021-03-28 09:50:52 -0500

    inquirer basic prompt set up

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+
+const questions = () => {
+    inquirer
+        .prompt([
+            {
+            // use "input" by default
+            type: "checkbox",  
+            message: "What's up man?",
+            choices: [
+                {name: "idk nm"},
+                {name: "workin' hard"},
+                {name: "hardly workin'"},
+            ],
+            name: "whatsup"
+            }
+        ])
+}
+
+questions()

254d48e5b63b73d015db5ac55e6202c828a717af
Ryan <ryanpburnett@yahoo.com>
2021-03-28 10:52:36 -0500

    fixed syntax errors in schema, added NOT NULL

--- db/schema.sql
+++ db/schema.sql
@@ -5,19 +5,22 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department (
-	id INT PRIMARY KEY,
-    name VARCHAR(30)
+	id INT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 CREATE TABLE role (
-	id INT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT
+	id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
 ); 
 CREATE TABLE employee (
-	id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT
+	id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id)
 );

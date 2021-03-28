9628931b8a509b847930a68a589b51d12de19e95
Ryan <ryanpburnett@yahoo.com>
2021-03-28 13:06:10 -0500

    foreign keys established

--- db/schema.sql
+++ db/schema.sql
@@ -7,14 +7,15 @@ USE employee_tracker_db;
 CREATE TABLE department (
 	id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY(id)
 );
 CREATE TABLE role (
 	id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id)
+    FOREIGN KEY(department_id) REFERENCES department(id)
+    PRIMARY KEY(id)
 ); 
 CREATE TABLE employee (
 	id INT NOT NULL AUTO_INCREMENT,
@@ -22,5 +23,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NOT NULL,
-    PRIMARY KEY (id)
+    FOREIGN KEY(role_id) REFERENCES role(id)
+    FOREIGN KEY(manager_id) REFERENCES employee(id)
+    PRIMARY KEY(id)
 );

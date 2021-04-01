6264ff64e827fbec615729cf616a50f0c0e82a86
Ryan <ryanpburnett@yahoo.com>
2021-04-01 10:19:13 -0500

    add employee now working properly

--- db/schema.sql
+++ db/schema.sql
@@ -10,11 +10,10 @@ CREATE TABLE department (
     PRIMARY KEY(id)
 );
 CREATE TABLE role (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    FOREIGN KEY(department_id) REFERENCES department(id),
     PRIMARY KEY(id)
 ); 
 CREATE TABLE employee (
@@ -23,7 +22,5 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NOT NULL,
-    FOREIGN KEY(role_id) REFERENCES role(id),
-    FOREIGN KEY(manager_id) REFERENCES employee(id),
     PRIMARY KEY(id)
 );
